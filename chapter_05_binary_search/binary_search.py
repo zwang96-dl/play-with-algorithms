3cf5abd316bf7129b208d4a830326074937a2c52
Zhe Wang <wangzhe.dut@gmail.com>
2018-11-02 00:16:50 -0700

    finished 5-9

--- chapter_05_binary_search/binary_search.py
+++ chapter_05_binary_search/binary_search.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class BST:
     class _Node:
         def __init__(self, key, value):
@@ -29,6 +32,138 @@ class BST:
         else:
             node.right = self._insert(node.right, key, value)
         return node
+
+    def contains(self, key):
+        return self._contains(self._root, key)
+
+    def _contains(self, node, key):
+        if not node:
+            return False
+        if key == node.key:
+            return True
+        elif key < node.key:
+            return self._contains(node.left, key)
+        else:
+            return self._contains(node.right, key)
+
+    def search(self, key):
+        return self._search(self._root, key)
+
+    def _search(self, node, key):
+        if not node:
+            return
+        if key == node.key:
+            return node
+        elif key < node.key:
+            return self._search(node.left, key)
+        else:
+            return self._search(node.right, key)
+
+    def pre_order(self):
+        self._pre_order(self._root)
+
+    def _pre_order(self, node):
+        if node:
+            print(node.key)
+            self._pre_order(node.left)
+            self._pre_order(node.right)
+
+    def in_order(self):
+        self._pre_order(self._root)
+
+    def _in_order(self, node):
+        if node:
+            self._in_order(node.left)
+            print(node.key)
+            self._in_order(node.right)
+
+    def post_order(self):
+        self._post_order(self._root)
+
+    def _post_order(self):
+        if node:
+            self._post_order(node.left)
+            self._post_order(node.right)
+            print(node.key)
+
+    def level_order(self):
+        queue = deque()
+        queue.append(self._root)
+        while queue:
+            curr = queue.popleft()
+            print(curr.value)
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+
+    def minimum(self):
+        assert self._count > 0
+        min_node = self._minimum(self._root)
+        return min_node.key
+
+    def _minimum(self, node):
+        if not node.left:
+            return node
+        return self._minimum(node.left)
+
+    def maximum(self):
+        assert self._count > 0
+        max_node = self._maximum(self._root)
+        return max_node.key
+
+    def _maximum(self, node):
+        if not node.right:
+            return node
+        return self._maximum(node.right)
+
+    def remove_min(self):
+        if self._root:
+            self._root = self._remove_min(self._root)
+
+    def _remove_min(self, node):
+        if not node.left:
+            right_node = node.right
+            self._count -= 1
+            return right_node
+        node.left = self._remove_min(node.left)
+        return node
+
+    def remove_max(self):
+        if self._root:
+            self._root = self._remove_max(self._root)
+
+    def _remove_max(self, node):
+        if not node.right:
+            left_node = node.left
+            self._count -= 1
+            return left_node
+        node.right = self._remove_max(node.right)
+        return node
+
+    def remove(self, key):
+        self._root = self._remove(self._root, key)
+
+    def _remove(self, node, key):
+        if not node:
+            return
+        if key < node.key:
+            node.left = self._remove(node.left, key)
+            return node
+        elif key > node.key:
+            node.right = self._remove(node.right, key)
+            return node
+        else:
+            if not node.left:
+                self._count -= 1
+                return node.right
+            if not node.right:
+                self._count -= 1
+                return node.left
+            successor = self._minimum(node.right)
+            successor.right = self._remove_min(node.right)
+            successor.left = node.left
+            return successor
 
 
 def binary_search(arr, target):
--- chapter_05_binary_search/main.py
+++ chapter_05_binary_search/main.py
@@ -1,5 +1,17 @@
-from chapter_05_binary_search.binary_search import binary_search
+from time import time
+from chapter_05_binary_search.binary_search import BST
 
 
 if __name__ == '__main__':
-    pass
+    with open('./chapter_05_binary_search/shakespeare.txt', 'r') as f:
+        words = f.read()
+    words = words.split()
+
+    start_time = time()
+    bst = BST()
+    for word in words:
+        if bst.contains(word):
+            bst.search(word).value += 1
+        else:
+            bst.insert(word, 1)
+    print('BST time cost: {} seconds'.format(time() - start_time))
